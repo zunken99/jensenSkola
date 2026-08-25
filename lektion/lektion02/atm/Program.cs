b8a55e23fee760ebf2ec4e4ff09b26c6df30253e
zunken99 <isakoaderu@gmail.com>
2026-08-25 14:08:13 +0200

    new atm

--- lektion/lektion02/atm/Program.cs
+++ lektion/lektion02/atm/Program.cs
@@ -2,9 +2,23 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        var test = "test";
-        Console.WriteLine(test);
+        // Deklarera en variabel för aktuellt saldo
+        int balance;
+        // Insättning
+        Console.WriteLine("Hur mycket vill du sätta in?");
+        // Deklarera en variabel för insättningen
+        var depositInput = Console.ReadLine();
+        
+        if (string.IsNullOrWhiteSpace(depositInput))
+        {
+            Console.WriteLine("Inget belopp angivet.");
+            Environment.Exit(0);
+        }
+
+        balance = int.Parse(depositInput);
+
+        Console.WriteLine($"Ditt saldo är nu: {balance} kr");
     }
 }
