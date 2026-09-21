2fd043fba4b4350635fbc2ef5156d41e90048a7d
zunken99 <isakoaderu@gmail.com>
2026-09-21 14:46:05 +0200

    update classes

--- grupparbete-08/WestcoastCars/customer.cs
+++ grupparbete-08/WestcoastCars/customer.cs
@@ -1,7 +1,10 @@
 namespace WestcoastCars;
 
 
-class Customer
+class Customer(int email, string deliveryAddress, string invoioceAdress, string phoneNumber)
 {
-    
+    public int Email { get; set; } = email;
+    public string DeliveryAddress { get; set; } = deliveryAddress;
+    public string InvoiceAddress { get; set; } = invoioceAdress;
+    public string PhoneNumber { get; set; } = phoneNumber;
 }
--- grupparbete-08/WestcoastCars/vehicle.cs
+++ grupparbete-08/WestcoastCars/vehicle.cs
@@ -1,1 +1,12 @@
 namespace WestcoastCars;
+
+class Vehicle(string make, string model, int year, string color, string mileage, string fuelType)
+{
+    public string Make { get; set; } = make;
+    public string Model { get; set; } = model;
+    public int Year { get; set; } = year;
+    public string Color { get; set; } = color;
+    public string Mileage { get; set; } = mileage;
+    public string FuelType { get; set; } = fuelType;
+}
+    
