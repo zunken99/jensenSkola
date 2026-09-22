d05ad5ff76a4b2085511da7b76a744b44d33586d
zunken99 <isakoaderu@gmail.com>
2026-09-22 13:54:58 +0200

    updated classes, added json save

---
+++ jsonTest/ReadAndWriteJson/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace ReadAndWriteJson;
+
+class Person
+{
+    public string FirstName { get; set; }= "";  
+    public string LastName { get; set; }= "";
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        string path = string .Concat(Environment.CurrentDirectory, "/data/person.json");
+
+        //Console.WriteLine(path);
+
+        Person person = new()
+        {
+            FirstName = "John",
+            LastName = "Doe"
+        };
+
+        var options = new JsonSerializerOptions()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+
+        };
+
+        var json = JsonSerializer.Serialize(person, options);
+
+        File.WriteAllText(path, json);
+
+        var jsonFromFile = File.ReadAllText(path);
+        //Console.WriteLine(jsonFromFile);
+
+        options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        Person? person1 = JsonSerializer.Deserialize<Person>(jsonFromFile, options)!;
+        Console.WriteLine(person1.FirstName);
+    }
+}
---
+++ lektion/ReadAndWriteFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ReadAndWriteFiles;
+
+class Program
+{
+    static void Main()
+    {
+        string path = Environment.CurrentDirectory + "/data/log.txt";
+        string message = "Info "+ DateTime.Now.ToString("yy-MM-dd HH:mm") + " - This is a log message.";
+        // File.AppendAllText(path, "\n" + message);
+
+        // string whatsInFile = File.ReadAllText(path);
+        // Console.WriteLine(whatsInFile);
+        using StreamWriter sw = new(path);
+        sw.WriteLine(message, true);
+        string whatsInFile = File.ReadAllText(path);
+       
+
+        sw.Close();
+        
+        using StreamReader sr = new(path);
+        string infoBack = sr.ReadToEnd();
+        Console.WriteLine(infoBack);
+    }
+}
