0edcfcb08a828aa1b360d7fc6676a3dd402e19ba
zunken99 <isakoaderu@gmail.com>
2026-09-22 14:34:43 +0200

    upd

--- grupparbete-08/WestcoastCars/Program.cs
+++ grupparbete-08/WestcoastCars/Program.cs
@@ -8,13 +8,14 @@ class Program
     {
         string jsonFilePath = string.Concat(Environment.CurrentDirectory, "/data/vehicle.json");
         string customerJsonFilePath = string.Concat(Environment.CurrentDirectory, "/data/customer.json");
+        
 
-        Car volvo = new Car("Volvo", "V90", 2020, "Black", "15000", "Diesel", "ABC123", "2.0L", "Automatic", "5");
-        Bike harley = new Bike("Harley-Davidson", "Street 750", 2019, "Red", "5000", "Petrol", "XYZ789", "Cruiser");
-        Truck scania = new Truck("Scania", "R500", 2021, "White", "20000", "Diesel", "TRK456", 20000, true, "12.7L", "Manual", "2");
+        Car volvo = new("Volvo", "V90", 2020, "Black", "15000", "Diesel", "ABC123", "2.0L", "Automatic", "5");
+        Bike harley = new("Harley-Davidson", "Street 750", 2019, "Red", "5000", "Petrol", "XYZ789", "Cruiser");
+        Truck scania = new("Scania", "R500", 2021, "White", "20000", "Diesel", "TRK456", 20000, true, "12.7L", "Manual", "2");
 
-        PrivateCustomer isak = new PrivateCustomer("isak", "svensson", "19900101-1234", 123456789, "Malmögatan Malmö", "Malmögatan Ystad", "0701234567");
-        OrganizationCustomer bosseBil = new OrganizationCustomer("Bosse bildoktor", "556677-8899", 987654321, "Storgatan Malmö", "Storgatan Ystad", "0709876543");
+        PrivateCustomer isak = new("isak", "svensson", "19900101-1234", 123456789, "Malmögatan Malmö", "Malmögatan Ystad", "0701234567");
+        OrganizationCustomer bosseBil = new("Bosse bildoktor", "556677-8899", 987654321, "Storgatan Malmö", "Storgatan Ystad", "0709876543");
 
         foreach (var vehicle in new Vehicle[] { volvo, harley, scania })
         {
@@ -25,8 +26,6 @@ class Program
         {
             customer.WriteCustomerInfoJSON(customerJsonFilePath);
         }
-
-        //DisplayVehicleInfo(volvo, harley, scania);
     }
 
     private static void DisplayVehicleInfo(Car volvo, Bike harley, Truck scania)
--- grupparbete-08/WestcoastCars/orgCustomer.cs
+++ grupparbete-08/WestcoastCars/orgCustomer.cs
@@ -7,6 +7,7 @@ public class OrganizationCustomer : Customer
 {
     public string OrganizationName { get; set; }
     public string OrganizationNumber { get; set; }
+    
 
     public OrganizationCustomer(string organizationName, string organizationNumber, int email, string deliveryAddress, string invoiceAddress, string phoneNumber)
         : base(email, deliveryAddress, invoiceAddress, phoneNumber)
